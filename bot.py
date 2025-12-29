f8744ef114f99c7e69c64a8eecfd67779b19a8db
golasola6 <golasola672@gmail.com>
2025-12-29 14:53:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ class Bot(Client):
          api_id=API_ID,
          api_hash=API_HASH,
          bot_token=BOT_TOKEN,
-         plugins=dict(root="plugins"),
+         plugins=dict(root="lazydeveloperr"),
          workers=50,
          sleep_threshold=10
         )
